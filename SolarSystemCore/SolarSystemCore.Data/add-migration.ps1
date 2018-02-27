8095c25a88706f93315b1cce4ccda4865aca7132
vshodi <shodimeji@gmail.com>
2018-02-27 19:48:29 +0000

    Make add migration script take mandatory migration name

--- SolarSystemCore/SolarSystemCore.Data/add-migration.ps1
+++ SolarSystemCore/SolarSystemCore.Data/add-migration.ps1
@@ -1,2 +1,2 @@
-param([string]$migrationName)
+param([Parameter(Mandatory=$true)][string]$migrationName)
 dotnet ef --project . --startup-project ../SolarSystemCore.WebApi migrations add $migrationName
